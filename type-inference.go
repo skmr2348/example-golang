31b1aa95cfc9dc0f20719e2ca9f49a294cfe0698
skmr2348 <sae.fj130@gmail.com>
2021-11-24 21:52:12 +0900

    type inference

---
+++ type-inference.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main(){
+	a:= 's'
+	fmt.Printf("a is of type %T\n", a)
+	b:= "test"
+	fmt.Printf("b is of type %T\n", b)
+	c:= 42
+	fmt.Printf("c is of type %T\n", c)
+	d:= 42.0
+	fmt.Printf("d is of type %T\n", d)
+}
