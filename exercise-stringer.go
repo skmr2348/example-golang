4a006d1fdb6536622b1167318eb0876c22edb0e1
skmr2348 <sae.fj130@gmail.com>
2021-11-25 13:55:32 +0000

    Add exercise-stringer.go

---
+++ exercise-stringer.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type IPAddr [4]byte
+
+func (ip IPAddr) String() string {
+	return fmt.Sprintf("%v.%v.%v.%v", ip[0], ip[1], ip[2], ip[2])
+}
+
+func main() {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v %v: %v\n", name, ip)
+	}
+
+}
+
+// hosts are not slice. range doesn't return index
